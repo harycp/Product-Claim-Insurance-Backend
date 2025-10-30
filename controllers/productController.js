53b39cf8229834daa435bd9e75af4ceccfc370c1
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-10-30 18:14:33 +0700

    feat: refactor return json data product

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,11 @@ const productService = require("../services/productService");
 const list = async (req, res) => {
   try {
     const result = await productService.listProducts();
-    return res.json(result);
+    return res.json({
+      status: 200,
+      message: "Products found",
+      data: result,
+    });
   } catch {
     return res.status(500).json({ message: "Internal Server Error" });
   }
@@ -13,7 +17,11 @@ const create = async (req, res) => {
   try {
     const result = await productService.createProduct(req.body);
     if (result.error) return res.status(400).json({ message: result.error });
-    return res.status(201).json(result);
+    return res.json({
+      status: 201,
+      message: "Product created successfully",
+      data: result,
+    });
   } catch {
     return res.status(500).json({ message: "Internal Server Error" });
   }
@@ -26,7 +34,11 @@ const buy = async (req, res) => {
       productId: req.params.id,
     });
     if (result.error) return res.status(404).json({ message: result.error });
-    return res.status(201).json(result);
+    return res.json({
+      status: 200,
+      message: "Product bought successfully",
+      data: result,
+    });
   } catch {
     return res.status(500).json({ message: "Internal Server Error" });
   }
